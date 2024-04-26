4a7199671c8bd30a00ae0158a0a61baa90c162e2
Muhammad Bilal <bmemon124@gmail.com>
2024-04-26 23:49:50 +0500

    back-end almost complete

--- controllers/list.js
+++ controllers/list.js
@@ -1,21 +1,77 @@
 const List = require("../models/listSchema")
 const userSchema = require("../models/signUpAndInSchema")
 
-async function userList(req, res) {
+// Create
+async function createUserList(req, res) {
     try {
         const { title, body, email } = req.body
         const existingUser = await userSchema.findOne({ email })
         if (existingUser) {
             const list = new List({ title, body, user: existingUser })
-            await list.save().then(() => res.status(200).json({ list }))
-            existingUser.UserSchema.push(list)
-            existingUser.save()
+            await list.save().then(() => {
+                existingUser.list.push(list)
+                existingUser.save()
+                return res.status(200).json({ list })
+            })
         }
     } catch (error) {
         console.log(error);
     }
 }
 
+// Update
+
+async function updateUserList(req, res) {
+    try {
+        const { title, body, email } = req.body
+        const existingUser = await userSchema.findOne({ email })
+        if (existingUser) {
+            const list = await List.findByIdAndUpdate(req.params.id, { title, body })
+            list.save().then(() => {
+                return res.status(200).json({ message: "Task Update" })
+            })
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+// Delete 
+
+async function deleteUserList(req, res) {
+    try {
+        const { email } = req.body
+        const existingUser = await userSchema.findOneAndUpdate({ email }, { $pull: { list: req.params.id } })
+        if (existingUser) {
+            await List.findByIdAndDelete(req.params.id)
+                .then(() => {
+                    return res.status(200).json({ message: "Delete Successfully" })
+                })
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+// get 
+
+async function getUserListById(req, res) {
+    try {
+        const list = await List.find({ user: req.params.id }).sort({ createdAt: -1 })
+        if (list.length !== 0) {
+            res.status(200).json({ list: list })
+        }else {
+            req.status(200).json({mesage: "No Tasks"})
+        }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
 module.exports = {
-    userList
+    createUserList,
+    updateUserList,
+    deleteUserList,
+    getUserListById
 }
--- routes/list.js
+++ routes/list.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const { userList } = require("../controllers/list")
+const { createUserList, updateUserList, deleteUserList,getUserListById } = require("../controllers/list")
 
-router.post("/addTask", userList)
+router.post("/addTask", createUserList)
+router.put("/updateTask/:id", updateUserList)
+router.delete("/deleteTask/:id", deleteUserList)
+router.get("/getUser/:id", getUserListById)
 
 module.exports = router;
