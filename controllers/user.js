1869a26816410be65f9405fa474b000cf79bee14
Muhammad Bilal <bmemon124@gmail.com>
2024-05-15 12:55:00 -0700

    forgot password api, send email api and otp api

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ const userSchema = require("../models/signUpAndInSchema")
 const List = require("../models/listSchema")
 const bcrypt = require("bcrypt")
 const nodemailer = require("nodemailer")
-const authSchema = require("../models/signUpAndInSchema")
+const Otp = require("../models/otp")
 
 // Sign Up
 async function signUpUser(req, res) {
@@ -75,19 +75,87 @@ async function getUserListsById(req, res) {
     }
 }
 
+// send email
 async function sendEmail(req, res) {
     try {
-        res.status(200).json({ message: "OK" })
+        let data = await userSchema.findOne({ email: req.body.email })
+        if (data) {
+            let otpCode = Math.floor((Math.random() * 10000) + 1);
+            let otpData = new Otp({
+                email: req.body.email,
+                code: otpCode,
+                expireIn: new Date().getTime() + 300 * 1000
+            })
+            await otpData.save()
+            return res.status(200).json({ message: 'Success Please Check Your Email' })
+        } else {
+            return res.status(200).json({ message: 'Email Not Exist' })
+        }
+        // return res.status(404).json({ message: message })
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
+
+// forgot password
+async function forgotPassword(req, res) {
+    try {
+        let data = await Otp.find({ email: req.body.email, code: req.body.otpCode })
+        if (data) {
+            let currentTime = new Date().getTime()
+            let difference = data.expireIn - currentTime
+            if (difference < 0) {
+                return res.status(200).json({ message: "Token Expire" })
+            } else {
+                let user = await userSchema.findOne({ email: req.body.email })
+                console.log("hellow world user: ", user);
+                if(!!user){
+                user.password = req.body.password
+                await user.save()
+                return res.status(200).json({ message: "Password Changed successfully" })
+            }
+            else return res.status(400).json({message: "User Not found"})
+        }
+        } else {
+            return res.status(400).json({ message: "Invalid Otp" })
+        }
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 }
 
 
+const mailer = (email, otp) => {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        port: "587",
+        secure: "false",
+        auth: {
+            user: "OTP@gmail.com",
+            pass: "otp"
+        }
+    })
+    const mailOptions = {
+        from: 'OTP@gmail.com',
+        to: 'bmemon124@gmail.com',
+        subject: 'Send Email Using Node.js ',
+        text: "Thank You Sir !"
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email Sent: ', info.response);
+        }
+    })
+}
+
 module.exports = {
     signUpUser,
     signInUser,
     chnagePassword,
     getUserListsById,
-    sendEmail
+    sendEmail,
+    forgotPassword
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const { signUpUser, signInUser, chnagePassword, sendEmail, getUserListsById } = require("../controllers/user")
+const { signUpUser, signInUser, chnagePassword, sendEmail, forgotPassword, getUserListsById } = require("../controllers/user")
 
 router.post("/register", signUpUser)
 router.post("/signin", signInUser)
-router.post("/forget/password", chnagePassword)
+router.post("/change/password", chnagePassword)
 router.post("/send/email", sendEmail)
+router.post("/forgot/password", forgotPassword)
 router.get("/todos/:id", getUserListsById)
 module.exports = router;
